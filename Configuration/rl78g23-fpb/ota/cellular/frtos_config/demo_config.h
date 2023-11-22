706a73319fce27d8e8e93483f55394541423c968
Tomohiko.Watanabe <tomohiko.watanabe.xz@bp.renesas.com>
2023-11-22 13:32:34 +0900

    enable logging for OTA demo.

--- Configuration/rl78g23-fpb/ota/cellular/frtos_config/demo_config.h
+++ Configuration/rl78g23-fpb/ota/cellular/frtos_config/demo_config.h
@@ -52,7 +52,7 @@
 #endif
 
 #ifndef LIBRARY_LOG_LEVEL
-#define LIBRARY_LOG_LEVEL    LOG_NONE
+#define LIBRARY_LOG_LEVEL    LOG_INFO
 #endif
 
 #include "iot_logging_task.h"
--- Demos/OtaOverMqtt/OtaOverMqttDemoExample.c
+++ Demos/OtaOverMqtt/OtaOverMqttDemoExample.c
@@ -1404,11 +1404,7 @@ void vStartOtaDemo( void )
                  "OTA Demo Task",          /* Text name for the task - only used for debugging. */
                  democonfigDEMO_STACKSIZE, /* Size of stack (in words, not bytes) to allocate for the task. */
                  NULL,                     /* Optional - task parameter - not used in this case. */
-#ifndef TOMO
                  tskIDLE_PRIORITY,         /* Task priority, must be between 0 and configMAX_PRIORITIES - 1. */
-#else
-                 tskIDLE_PRIORITY + 1,     /* Task priority, must be between 0 and configMAX_PRIORITIES - 1. */
-#endif
                  NULL );                   /* Optional - used to pass out a handle to the created task. */
 }
 
