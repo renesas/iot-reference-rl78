07dfe5bd92d75534e794c8b4aab354eff5f2da9b
Tomohiko.Watanabe <tomohiko.watanabe.xz@bp.renesas.com>
2024-01-16 19:54:46 +0900

    Set mqttexampleTRANSPORT_SEND_RECV_TIMEOUT_MS to 450.

--- Demos/mqtt_agent/mqtt_agent_task.c
+++ Demos/mqtt_agent/mqtt_agent_task.c
@@ -200,7 +200,7 @@
 /**
  * @brief Socket send and receive timeouts to use.  Specified in milliseconds.
  */
-#define mqttexampleTRANSPORT_SEND_RECV_TIMEOUT_MS    ( 200 )
+#define mqttexampleTRANSPORT_SEND_RECV_TIMEOUT_MS    ( 450 )
 
 /**
  * @brief Configuration is used to turn on or off persistent sessions with MQTT broker.
